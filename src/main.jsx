4b0477a206d225d94255a8b6217b067c8bb565fb
Héctor David Aguirre <hdaguirre@herrera.unt.edu.ar>
2026-04-03 22:07:17 -0400

    agregar panel de administracion

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Admin from './Admin.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const isAdmin = window.location.pathname === '/admin'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {isAdmin ? <Admin /> : <App />}
+  </React.StrictMode>
 )
