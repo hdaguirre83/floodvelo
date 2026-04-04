a87404d598e2679be57c54c0b1ab8777f11befea
Héctor David Aguirre <hdaguirre@herrera.unt.edu.ar>
2026-04-04 13:27:25 -0400

    fix router mapa

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Admin from './Admin.jsx'
+import MapaEventos from './MapaEventos.jsx'
 
-const isAdmin = window.location.pathname === '/admin'
+Const path = window.location.pathname
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {isAdmin ? <Admin /> : <App />}
+    {path === '/admin' ? <Admin /> : path === '/mapa' ? <MapaEventos /> : <App />}
   </React.StrictMode>
 )
