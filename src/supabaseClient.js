da2f0240575f43a4f24e0684920da8d4956b4863
Héctor David Aguirre <hdaguirre@herrera.unt.edu.ar>
2026-04-03 21:28:32 -0400

    usar variables de entorno

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = "https://qfobadmakzahoejydsdy.supabase.co"
-const SUPABASE_KEY = "sb_publishable_S0V-8iTwWAY5xydsj2CFDQ_tJ2d9ubF"
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
