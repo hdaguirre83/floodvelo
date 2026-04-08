503b358bef32e315238e1edf0c81c941da15c32f
Héctor David Aguirre <hdaguirre@herrera.unt.edu.ar>
2026-04-07 22:08:42 -0400

    prueba modif supabase client

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,11 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://qfobadmakzahoejydsdy.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmb2JhZG1ha3phaG9lanlkc2R5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMDAxMTQsImV4cCI6MjA5MDY3NjExNH0.kjNDzBjLgAPYum9M150i0my_LrxtU7JQGuAWZF8E5Zc' // ⚠️ Reemplazá con tu clave real
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error('Missing Supabase environment variables');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+console.log("Cliente Supabase creado. URL:", supabaseUrl);
